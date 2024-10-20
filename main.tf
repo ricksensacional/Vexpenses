d2f323945243615ba133139b00649ffd24959ab6
ricksensacional <146139310+ricksensacional@users.noreply.github.com>
2024-10-20 13:05:35 -0300

    main.tf

--- main.tf
+++ main.tf
@@ -14,9 +14,67 @@ variable "candidato" {
   default     = "SeuNome"
 }
 
+resource "aws_kms_key" "s3_kms_key" {
+  description             = "Chave KMS para criptografar o bucket S3"
+  deletion_window_in_days = 10
+
+  tags = {
+    Name = "${var.projeto}-${var.candidato}-kms-key"
+  }
+}
+
+resource "aws_kms_alias" "s3_kms_alias" {
+  name          = "alias/${var.projeto}-${var.candidato}-kms"
+  target_key_id = aws_kms_key.s3_kms_key.id
+}
+
+# Bucket S3 com criptografia KMS
+resource "aws_s3_bucket" "main_s3_bucket" {
+  bucket = "${var.projeto}-${var.candidato}-bucket"
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm     = "aws:kms"
+        kms_master_key_id = aws_kms_key.s3_kms_key.arn
+      }
+    }
+  }
+
+  tags = {
+    Name = "${var.projeto}-${var.candidato}-s3"
+  }
+}
+
+# Permissão para a chave KMS no bucket S3
+resource "aws_s3_bucket_policy" "main_s3_bucket_policy" {
+  bucket = aws_s3_bucket.main_s3_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid       = "AllowSSLRequestsOnly",
+        Effect    = "Deny",
+        Principal = "*",
+        Action    = "s3:*",
+        Resource  = [
+          "${aws_s3_bucket.main_s3_bucket.arn}",
+          "${aws_s3_bucket.main_s3_bucket.arn}/*"
+        ],
+        Condition = {
+          Bool = {
+            "aws:SecureTransport" = "false"
+          }
+        }
+      }
+    ]
+  })
+}
+
 resource "tls_private_key" "ec2_key" {
   algorithm = "RSA"
-  rsa_bits  = 2048
+  rsa_bits  = 4096
 }
 
 resource "aws_key_pair" "ec2_key_pair" {
@@ -76,16 +134,16 @@ resource "aws_route_table_association" "main_association" {
 
 resource "aws_security_group" "main_sg" {
   name        = "${var.projeto}-${var.candidato}-sg"
-  description = "Permitir SSH de qualquer lugar e todo o tráfego de saída"
+  description = "Permitir SSH de IPs específicos e todo o tráfego de saída"
   vpc_id      = aws_vpc.main_vpc.id
 
   # Regras de entrada
   ingress {
-    description      = "Allow SSH from anywhere"
+    description      = "Allow SSH from specific IP"
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    cidr_blocks      = ["192.168.1.5/22"]
     ipv6_cidr_blocks = ["::/0"]
   }
 
@@ -102,6 +160,48 @@ resource "aws_security_group" "main_sg" {
   tags = {
     Name = "${var.projeto}-${var.candidato}-sg"
   }
+}
+# Criar uma política de IAM para a instância EC2 
+resource "aws_iam_role" "ec2_role" {
+  name = "${var.projeto}-${var.candidato}-ec2-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Effect = "Allow"
+        Sid    = ""
+      },
+    ]
+  })
+}
+
+resource "aws_iam_policy" "ec2_policy" {
+  name        = "${var.projeto}-${var.candidato}-ec2-policy"
+  description = "Política para restringir acesso à instância EC2"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:ListBucket",
+          "s3:GetObject"
+        ]
+        Resource = "*"
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ec2_role_attachment" {
+  policy_arn = aws_iam_policy.ec2_policy.arn
+  role       = aws_iam_role.ec2_role.name
 }
 
 data "aws_ami" "debian12" {
@@ -126,19 +226,23 @@ resource "aws_instance" "debian_ec2" {
   subnet_id       = aws_subnet.main_subnet.id
   key_name        = aws_key_pair.ec2_key_pair.key_name
   security_groups = [aws_security_group.main_sg.name]
-
+   iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
   associate_public_ip_address = true
 
   root_block_device {
     volume_size           = 20
     volume_type           = "gp2"
     delete_on_termination = true
+    encrypted             = true
   }
 
   user_data = <<-EOF
               #!/bin/bash
               apt-get update -y
               apt-get upgrade -y
+              apt-get install nginx -y
+              systemctl start nginx
+              systemctl enable nginx
               EOF
 
   tags = {
@@ -155,4 +259,8 @@ output "private_key" {
 output "ec2_public_ip" {
   description = "Endereço IP público da instância EC2"
   value       = aws_instance.debian_ec2.public_ip
-}
+} 
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = "${var.projeto}-${var.candidato}-ec2-profile"
+  role = aws_iam_role.ec2_role.name
+  }
